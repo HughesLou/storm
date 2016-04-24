22be6fe951d93cd877cf16a63a4dcfdc48463c9f
Hughes <Luzi2000lh@gmail.com>
2016-04-24 21:41:01 +0800

    Get familiar with Storm

--- examples/storm-starter/src/jvm/org/apache/storm/starter/ExclamationTopology.java
+++ examples/storm-starter/src/jvm/org/apache/storm/starter/ExclamationTopology.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,65 +23,76 @@ import org.apache.storm.StormSubmitter;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.testing.TestWordSpout;
+import org.apache.storm.topology.BoltDeclarer;
 import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.SpoutDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * This is a basic example of a Storm topology.
  */
 public class ExclamationTopology {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExclamationTopology.class);
 
-  public static class ExclamationBolt extends BaseRichBolt {
-    OutputCollector _collector;
+    public static class ExclamationBolt extends BaseRichBolt {
+        OutputCollector _collector;
 
-    @Override
-    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-      _collector = collector;
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple tuple) {
+            String id = Thread.currentThread().getName().substring(27);
+            String value = tuple.getString(0) + "-" + id.substring(1, id.length() / 2);
+            List<Integer> ids = _collector.emit(tuple, new Values(value));
+
+            if (ids.isEmpty()) {
+                LOGGER.info("{}", value);
+            }
+//            LOGGER.info("Value {} is sent to {}", value, StringUtils.join(ids, ":"));
+            _collector.ack(tuple);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("bword"));
+        }
     }
 
-    @Override
-    public void execute(Tuple tuple) {
-      _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
-      _collector.ack(tuple);
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        SpoutDeclarer spoutDeclarer = builder.setSpout("words", new TestWordSpout(), 4);
+        BoltDeclarer boltDeclarer1 = builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("words");
+        BoltDeclarer boltDeclarer2 = builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
+        BoltDeclarer boltDeclarer3 = builder.setBolt("exclaim3", new ExclamationBolt(), 5).shuffleGrouping("words");
+
+        Config conf = new Config();
+//        conf.setDebug(true);
+
+        if (null != args && args.length > 0) {
+            conf.setNumWorkers(3);
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        } else {
+            LocalCluster cluster = new LocalCluster();
+            LOGGER.info("Submit topology...");
+            cluster.submitTopology("test", conf, builder.createTopology());
+            Utils.sleep(10000);
+            LOGGER.info("Running OK...");
+            cluster.killTopology("test");
+            cluster.shutdown();
+        }
     }
-
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word"));
-    }
-
-
-  }
-
-  public static void main(String[] args) throws Exception {
-    TopologyBuilder builder = new TopologyBuilder();
-
-    builder.setSpout("word", new TestWordSpout(), 10);
-    builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
-    builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
-
-    Config conf = new Config();
-    conf.setDebug(true);
-
-    if (args != null && args.length > 0) {
-      conf.setNumWorkers(3);
-
-      StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
-    }
-    else {
-
-      LocalCluster cluster = new LocalCluster();
-      cluster.submitTopology("test", conf, builder.createTopology());
-      Utils.sleep(10000);
-      cluster.killTopology("test");
-      cluster.shutdown();
-    }
-  }
 }
--- storm-core/src/jvm/org/apache/storm/testing/TestWordSpout.java
+++ storm-core/src/jvm/org/apache/storm/testing/TestWordSpout.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,19 +17,21 @@
  */
 package org.apache.storm.testing;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.storm.Config;
-import org.apache.storm.topology.OutputFieldsDeclarer;
-import java.util.Map;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
-import java.util.HashMap;
-import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class TestWordSpout extends BaseRichSpout {
@@ -44,43 +46,45 @@ public class TestWordSpout extends BaseRichSpout {
     public TestWordSpout(boolean isDistributed) {
         _isDistributed = isDistributed;
     }
-        
+
+    @Override
     public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
         _collector = collector;
     }
-    
-    public void close() {
-        
-    }
-        
+
+    @Override
+    public void close() { }
+
+    @Override
     public void nextTuple() {
         Utils.sleep(100);
-        final String[] words = new String[] {"nathan", "mike", "jackson", "golda", "bertels"};
-        final Random rand = new Random();
-        final String word = words[rand.nextInt(words.length)];
-        _collector.emit(new Values(word));
-    }
-    
-    public void ack(Object msgId) {
-
+//        final String[] words = new String[]{"nathan", "mike", "jackson", "golda", "bertels"};
+//        final Random rand = new Random();
+//        final String word = words[rand.nextInt(words.length)];
+        String id = Thread.currentThread().getName().substring(27);
+        List<Integer> ids = _collector.emit(new Values(id.substring(1, id.length() / 2)), "HUGHES");
+        LOG.info("Tuple is sent ot {}", StringUtils.join(ids, ","));
     }
 
-    public void fail(Object msgId) {
-        
-    }
-    
+    @Override
+    public void ack(Object msgId) { }
+
+    @Override
+    public void fail(Object msgId) { }
+
+    @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("word"));
+        declarer.declare(new Fields("sword"));
     }
 
     @Override
     public Map<String, Object> getComponentConfiguration() {
-        if(!_isDistributed) {
+        if (!_isDistributed) {
             Map<String, Object> ret = new HashMap<String, Object>();
             ret.put(Config.TOPOLOGY_MAX_TASK_PARALLELISM, 1);
             return ret;
         } else {
             return null;
         }
-    }    
+    }
 }
